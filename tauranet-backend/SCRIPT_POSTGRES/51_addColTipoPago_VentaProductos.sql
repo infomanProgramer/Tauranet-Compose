7edfa08741f69a90a79e902c6fedb964bff952be
unknown <infoman.rafael@gmail.com>
2025-06-18 12:08:16 -0400

    se añadio tauranet-backend y tauranet-frontend

---
+++ tauranet-backend/SCRIPT_POSTGRES/51_addColTipoPago_VentaProductos.sql
@@ -0,0 +1,2 @@
+ALTER TABLE venta_productos 
+ADD COLUMN tipo_pago INTEGER;
---
+++ tauranet-frontend/vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  devServer: {
+    watchOptions: {
+      poll: 1000, // revisa cambios cada segundo
+    },
+  },
+};
